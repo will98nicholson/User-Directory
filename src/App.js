e12992149d787a9c7f3b0f593fe5bb6ee0e676db
William Nicholson <will98nicholson@gmail.com>
2021-06-04 15:18:56 -0500

    handleinputchange

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import Table from './components/Table';
 
-function App() {
-  return (
-    <div className="App">
-      < SearchBar ></SearchBar>
-      < Table></Table>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    input: ""
+  };
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div className="App" >
+        < SearchBar handleInputChange={this.handleInputChange} ></SearchBar>
+        < Table></Table>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,23 +1,14 @@
 import Form from 'react-bootstrap/Form';
 
-export default function Searchbar() {
-    const handleInputChange = event => {
-        // Getting the value and name of the input which triggered the change
-        let value = event.target.value;
-        const name = event.target.name;
+export default function Searchbar(props) {
 
-        // Updating the input's state
-        this.setState({
-            [name]: value
-        });
-    };
 
     return (
         <div>
             <Form>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control name="input" placeholder="enter employee" onChange={props.handleInputChange} />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
     </Form.Text>
