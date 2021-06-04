109e8bdfcbe4e8191f8cae3301392c53e1618c5d
William Nicholson <will98nicholson@gmail.com>
2021-06-04 14:51:12 -0500

    table component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/SearchBar';
+import Table from './components/Table';
 
 function App() {
   return (
     <div className="App">
       < SearchBar ></SearchBar>
+      < Table></Table>
     </div>
   );
 }
---
+++ src/components/Table/index.js
@@ -0,0 +1,26 @@
+import Table from 'react-bootstrap/Table'
+export default function Usertable() {
+    return (
+        <div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+
+                </tbody>
+            </Table>
+        </div>
+    )
+}
